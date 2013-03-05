491065c9c888ae9075e403045ab987c8b0cc9f0b
Michael Critz <github@micahelcritz.com>
2013-03-05 14:48:05 -0800

    created staging environment. modified gitignore. added production credentials.js

---
+++ credentials.js
@@ -0,0 +1,9 @@
+var	user = 'mcritz46vl@gmail.com';
+var	pass = 'y3ll0wb4ll';
+var sender = 'WWDC Ticket Updates <wwdc_updates@michaelcritz.com>';
+var recipients = 'Critz WWDC List <wwdc_list@michaelcritz.com>';
+
+module.exports.user = user
+module.exports.pass = pass;
+module.exports.sender = sender;
+module.exports.recipients = recipients;
