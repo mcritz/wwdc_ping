e547891a1faa141f3033b4b4cd10989fc62b439f
Michael Critz <github@micahelcritz.com>
2013-03-05 12:29:23 -0800

    added comments.

--- getUrl.js
+++ getUrl.js
@@ -1,27 +1,32 @@
-var https = require('https');
-var searchRegExp = new RegExp("wwdc2012-june-11-15.jpg");
+var https = require('https'); // WWDC site is https. Besides… http://www.codinghorror.com/blog/2012/02/should-all-web-traffic-be-encrypted.html
+var urlToPing = 'https://developer.apple.com/WWDC/'; // WWDC site
+var searchRegExp = new RegExp('wwdc2012-june-11-15.jpg'); // old WWDC image
+var interval = 30 * 1000; // milliseconds
 
 notifyOnMatch = function(str){
+	// XXTODOXX: set up email feature.
 	console.log(str);
 	console.log('Matched!');
 }
 
 notifyOnFail = function(e){
-  console.log("Got error: " + e.message);
+	// XXTODOXX: more robust error handling
+	console.log('Got error: ' + e.message);
 }
 
 getUrl = function(){
-	https.get("https://developer.apple.com/WWDC/", function(res) {
-		var matched = false,
-			output = '';
+	// fetch
+	https.get(urlToPing, function(res) {
+		var output = '';
 
-		console.log("Got response: " + res.statusCode);
+		console.log('Got response: ' + res.statusCode);
 
 		res.on('data', function (chunk) {
 	        output += chunk;
 		});
 
 		res.on('end', function(){
+			// Check if site contains the old stuff
 			if (!searchRegExp.exec(output)) {
 				notifyOnMatch(output);
 			}
@@ -31,6 +36,7 @@ getUrl = function(){
 	});
 }
 
+// run on launch…
 getUrl();
-
-setInterval(getUrl, 30 * 1000);
+// then repeat at interval
+setInterval(getUrl, interval);
