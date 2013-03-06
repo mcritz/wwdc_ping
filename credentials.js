9c94bf5ff95560b7eb1144a77c6563f1a32b96da
Michael Critz <github@micahelcritz.com>
2013-03-05 18:15:18 -0800

    slightly smarter error codes

--- credentials.js
+++ credentials.js
@@ -1,9 +1,9 @@
-var	user = 'mcritz46vl@gmail.com';
-var	pass = 'y3ll0wb4ll';
-var sender = 'WWDC Ticket Updates <wwdc_updates@michaelcritz.com>';
-var recipients = 'Critz WWDC List <wwdc_list@michaelcritz.com>';
+var	user = 'your@gmail.com';
+var	pass = 'fancypassword';
+var sender = user;
+var recipients = user;
 
-module.exports.user = user
+module.exports.user = user;
 module.exports.pass = pass;
 module.exports.sender = sender;
 module.exports.recipients = recipients;
